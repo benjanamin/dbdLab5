e851f9a05553664557e6b4bdb26c6bc999e1a1d3
benja <benjamincastillo97@hotmail.com>
2020-08-16 10:32:20 -0400

    controlador categoria y advertisement

--- app/Http/Controllers/AdvertisementController.php
+++ app/Http/Controllers/AdvertisementController.php
@@ -3,8 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Advertisement;
 
 class AdvertisementController extends Controller
 {
-    //
+    public function index(){
+        $advertisements = Advertisement::all();
+        return $advertisements; 
+    }
+
+    public function create(){
+
+    }
+
+    public function store(Request $request){
+        $anuncio = new Advertisement;
+        $anuncio->Titulo = $request->Titulo;
+        $anuncio->Cantidad = $request->Cantidad;
+        $anuncio->Descripcion = $request->Descripcion;
+        $anuncio->PrecioUnitario = $request->PrecioUnitario;
+        $anuncio->RUTUsuario = $request->RUTUsuario;
+        $anuncio->IDCategoria = $anuncio->IDCategoria;
+        $anuncio->save();
+        return Advertisement::all(); 
+    }
+
+    public function show($id){
+        $anuncio = Advertisement::findOrFail($id);
+        return $anuncio;
+    }
+
+    public function edit($id){
+
+    }
+
+    public function update(Request $request, $id){
+        $anuncio = Advertisement::findOrFail($id);
+        $anuncio = new Advertisement;
+        $anuncio->Titulo = $request->Titulo;
+        $anuncio->Cantidad = $request->Cantidad;
+        $anuncio->Descripcion = $request->Descripcion;
+        $anuncio->PrecioUnitario = $request->PrecioUnitario;
+        $anuncio->RUTUsuario = $request->RUTUsuario;
+        $anuncio->IDCategoria = $anuncio->IDCategoria;
+        $anuncio->save();
+        return Advertisement::findOrFail($id);
+    }
+
+    public function destroy($id){
+        $anuncio = Advertisement::findOrFail($id);
+        $anuncio->delete();
+        return Advertisement::all();
+    }
 }
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -16,6 +16,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/*'
+        '*'
     ];
 }
